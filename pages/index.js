7e2e2cd3dc048485420e4e6c578dc75646abea49
Brandon Hawi <brandonhawi@jasonbusiness.com>
2020-07-26 14:54:12 -0700

    Fixed up UI a lot more

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,24 @@ import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getAllChores } from '../lib/chores'
 import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import { Card, CardContent, Typography } from '@material-ui/core/'
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { AppBar, Toolbar, IconButton, Fab } from '@material-ui/core/'
+import MenuIcon from '@material-ui/icons/Menu'
+import AddIcon from '@material-ui/icons/Add';
+import { spacing } from '@material-ui/system';
 
 const useStyles = makeStyles({
   table: {
     minWidth: 650,
   },
+  appBar: {
+    top: 'auto',
+    bottom: 0,
+  },
+  card: {
+    margin: 10,
+  }
 });
 
 export async function getStaticProps () {
@@ -27,6 +33,7 @@ export async function getStaticProps () {
 }
 export default function Home ({ allChoresData }) {
   const classes = useStyles();
+  const onMobile = useMediaQuery('only screen and (max-width: 768px)');
 
   return (
     <Layout home>
@@ -41,29 +48,28 @@ export default function Home ({ allChoresData }) {
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Chore List</h2>
-        <TableContainer component={Paper}>
-          <Table className={classes.table} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Roommate</TableCell>
-                <TableCell>Chore Name</TableCell>
-                <TableCell>Description</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
               {allChoresData.map(({ first_name, last_name, chore_name, chore_description }) => (
-                <TableRow key={chore_name}>
-                  <TableCell component="th" scope="row">
-                    {first_name} {last_name}
-                  </TableCell>
-                  <TableCell>{chore_name}</TableCell>
-                  <TableCell>{chore_description}</TableCell>
-                </TableRow>
+                <Card className={classes.card} key={chore_name}>
+                  <CardContent>
+                    <Typography variant="h5" gutterBottom>{first_name} {last_name}</Typography>
+                    {chore_name}
+                  </CardContent>
+                </Card>
               ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
       </section>
+      { onMobile ? (
+        <AppBar position="fixed" color="primary" className={classes.appBar}>
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="open drawer">
+            <MenuIcon />
+          </IconButton>
+          <Fab color="secondary" aria-label="add" className={classes.fabButton}>
+            <AddIcon />
+          </Fab>
+          <div className={classes.grow} />
+        </Toolbar>
+      </AppBar>
+      ) : null}
     </Layout>
   );
 }
