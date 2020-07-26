4172354297ef53a21d88b2091434d1d7bf540460
Brandon Hawi <brandonhawi@jasonbusiness.com>
2020-07-26 09:51:48 -0700

    Styling changes

--- pages/index.js
+++ pages/index.js
@@ -17,18 +17,17 @@ export default function Home ({ allChoresData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>
-          (This is a serverless application meant to keep track of weekly chores, brought to you by your favorite software/electrical engineers.
-          <a href='https://github.com/tech-boyz'>Our source code</a>.)
+        <p className={utilStyles.lightText}>
+          (This is a serverless application meant to keep track of weekly chores, brought to you by your favorite software/electrical engineers.)
         </p>
+        <a href='https://github.com/tech-boyz'>Our source code</a>.
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Chore List</h2>
         <table className={utilStyles.table}>
           {allChoresData.map(({ first_name, last_name, chore_name, chore_description }) => (
             <tr className={utilStyles.tableEntry} key={chore_name}>
-              <td>{first_name}</td>
-              <td>{last_name}</td>
+              <td>{first_name} {last_name}</td>
               <td>{chore_name}</td>
               <td>{chore_description}</td>
             </tr>
